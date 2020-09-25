57fe01ebafce0956ccdfb819c164ca4851c8cd9d
Voryzen <piro.travesura@gmail.com>
2020-09-25 16:01:41 +1000

    Update LifecyleTasks.sh

--- tasks/LifecyleTasks.sh
+++ tasks/LifecyleTasks.sh
@@ -17,6 +17,7 @@ Task::deploy(){
   Task::run_docker ansible-playbook $(debug_check) \
   --extra-vars="@$_config_dir/config.yml" --extra-vars="@$_config_dir/vault.yml" \
   -i inventory playbook.vivumlab.yml || colorize light_red "error: deploy"
+  
 }
 
 # Restart All Enabled services
@@ -36,6 +37,7 @@ Task::restart(){
   Task::run_docker ansible-playbook $(debug_check) \
   --extra-vars="@$_config_dir/config.yml" --extra-vars="@$_config_dir/vault.yml" \
   -i inventory playbook.restart.yml || colorize light_red "error: restart"
+  highlight "Services restarted"
 }
 
 # Restart the selected service
@@ -48,6 +50,7 @@ Task::restart_one(){
   Task::run_docker ansible-playbook $(debug_check) \
   --extra-vars="@$_config_dir/config.yml" --extra-vars="@$_config_dir/vault.yml" \
   --extra-vars='{"services":["'${_service}'"]}' -i inventory playbook.restart.yml || colorize light_red "error: restart_one"
+  highlight "Restarted ${_service}"
 }
 
 # Stop All Enabled services
@@ -67,6 +70,7 @@ Task::stop(){
   Task::run_docker ansible-playbook $(debug_check) \
   --extra-vars="@$_config_dir/config.yml" --extra-vars="@$_config_dir/vault.yml" \
   -i inventory playbook.stop.yml || colorize light_red "error: stop"
+  highlight "Services stopped"
 }
 
 # Stop the selected service
@@ -86,6 +90,7 @@ Task::stop_one(){
   Task::run_docker ansible-playbook $(debug_check) \
   --extra-vars="@$_config_dir/config.yml" --extra-vars="@$_config_dir/vault.yml" \
   --extra-vars='{"services":["'${_service}'"]}' -i inventory playbook.stop.yml  || colorize light_red "error: stop_one"
+  highlight "Stopped ${_service}"
 }
 
 # Removes One Service
@@ -106,7 +111,7 @@ Task::remove_one(){
   Task::run_docker ansible-playbook $(debug_check) \
   --extra-vars="@$_config_dir/config.yml" --extra-vars="@$_config_dir/vault.yml" \
   --extra-vars='{"services":["'${_service}'"]}' -i inventory playbook.remove.yml || colorize light_red "error: remove_one"
-}
+  highlight "Removed $"{_service}"
 
 # Resets a services' data files
 Task::reset_one(){
@@ -126,11 +131,12 @@ Task::reset_one(){
   Task::run_docker ansible-playbook $(debug_check) \
   --extra-vars="@$_config_dir/config.yml" --extra-vars="@$_config_dir/vault.yml" \
   --extra-vars='{"services":["'${_service}'"]}' -i inventory playbook.stop.yml || colorize light_red "error: reset_one: stop"
-	Task::run_docker ansible-playbook $(debug_check) \
+  Task::run_docker ansible-playbook $(debug_check) \
   --extra-vars="@$_config_dir/config.yml" --extra-vars="@$_config_dir/vault.yml" \
   --extra-vars='{"services":["'${_service}'"]}' -i inventory playbook.remove.yml  || colorize light_red "error: reset_one: remove"
-	highlight "Redeploying ${_service}"
-	Task::run_docker ansible-playbook $(debug_check) \
+  highlight "Redeploying ${_service}"
+  Task::run_docker ansible-playbook $(debug_check) \
   --extra-vars="@$_config_dir/config.yml" --extra-vars="@$_config_dir/vault.yml" \
   --extra-vars='{"services":["'${_service}'"]}' -i inventory -t deploy playbook.vivumlab.yml || colorize light_red "error: reset_one: deploy"
+  highlight "Reset ${_service}"
 }
