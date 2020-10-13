5182f1c43fef75c71bd0f187e5016c6c27e54c0f
Voryzen <piro.travesura@gmail.com>
2020-10-14 00:57:25 +1000

    Update UserActionTasks.sh

--- tasks/UserActionTasks.sh
+++ tasks/UserActionTasks.sh
@@ -74,3 +74,30 @@ Task::create_sshkey() {
   echo "Copying keys over to the machine, located at $(vlab_ip)"
   ssh-copy-id -i "$HOME/.ssh/$(pwless_sshkey).pub" "$(vlab_ssh_user)@$(vlab_ip) -p $(vlab_port)" || colorize light_red "error: create_sshkey: copying keys"
 }
+
+# Opens a shell in the VivumLab container
+Task::find_help() {
+  : @desc "Shows the user where to find help/ contact the VivumLab community"
+  
+  cat vivumlablogo.txt
+  echo ""
+  printf "MOTD:\n\n" && cat MOTD || printf "Could not get MOTD"
+  printf "\n\n"
+  
+  echo "Thank you for trying to find help.. the VivumLab community is here for you"
+  sleep 2
+  echo " You have some options: Zulip chat, Reddit, and Github"
+  sleep 2
+  echo "Help is most readily available via VivumLab Zulip chat"
+  colorize yellow "visit https://vivumlab.zulipchat.com"
+  sleep 1
+  echo "If help isn't urgently required, or you may not be able to reply promptly:"
+  colorize yellow "try https://reddit.com/r/vivumlab"
+  sleep 1
+  echo "please note that further input may be required from you."
+  echo "In this case, VivumLab and it's community will benefit from your presence at:"
+  colorize yellow "https://github.com/VivumLab/VivumLab"
+  sleep 1
+  echo "submit or view issues at:"
+  colorize yellow "https://github.com/VivumLab/VivumLab/issues"
+}
