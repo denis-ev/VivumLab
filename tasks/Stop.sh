345921b22e6c964e752be4db5633185aaac6ba30
Voryzen <piro.travesura@gmail.com>
2020-12-01 17:25:12 +1000

    Create Stop.sh

---
+++ tasks/Stop.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+### ALL SERVICES ###
+# Stop
+Task::stop(){
+  : @desc "Stops all enabled services"
+  : @param config_dir="settings"
+  : @param force true "Forces a rebuild/repull of the docker image"
+  : @param build true "Forces to build the image locally"
+  : @param debug true "Debugs ansible-playbook commands"
+  : @param cache true "Allows the build to use the cache"
+
+  Task::logo
+  Task::build $(build_check) $(force_check) $(cache_check)
+  Task::git_sync
+  Task::config
+
+  highlight "Stopping all services"
+  Task::run_docker ansible-playbook $(debug_check) $(sshkey_path) \
+  --extra-vars="@$_config_dir/config.yml" --extra-vars="@$_config_dir/vault.yml" \
+  -i inventory playbook.stop.yml || colorize light_red "error: stop"
+  highlight "Services stopped"
+}
+
+### SPECIFIC SERVICE ###
+# Stop
+Task::stop_one(){
+  : @desc "Stops the specified service"
+  : @param service! "Service Name"
+  : @param config_dir="settings"
+  : @param force true "Forces a rebuild/repull of the docker image"
+  : @param build true "Forces to build the image locally"
+  : @param debug true "Debugs ansible-playbook commands"
+  : @param cache true "Allows the build to use the cache"
+
+  Task::logo
+  Task::build $(build_check) $(force_check) $(cache_check)
+  Task::git_sync
+  Task::config
+
+  Task::run_docker ansible-playbook $(debug_check) $(sshkey_path) \
+  --extra-vars="@$_config_dir/config.yml" --extra-vars="@$_config_dir/vault.yml" \
+  --extra-vars='{"services":["'${_service}'"]}' -i inventory playbook.stop.yml  || colorize light_red "error: stop_one"
+  highlight "Stopped ${_service}"
+}
