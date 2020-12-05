5a8a2b27336f5d844951385d92b8340e6dc33489
Kevin Poorman <kjp@codefriar.com>
2020-12-05 01:18:35 -0500

    WIP Thor based CLI

---
+++ tasks/docs.rb
@@ -0,0 +1,14 @@
+module App
+  class Docs < SubCommandBase
+
+    desc "create", "create docs"
+    def create
+      # create
+    end
+
+    desc "publish", "publish docs"
+    def publish
+      # publish
+    end
+  end
+end
