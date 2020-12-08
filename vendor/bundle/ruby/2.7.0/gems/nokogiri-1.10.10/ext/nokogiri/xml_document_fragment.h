8c13ce50b944d7ed4da51591d4a61038206f870b
Kevin Poorman <kjp@codefriar.com>
2020-12-08 12:22:20 -0500

    new gems, and sanity checks complete.

---
+++ vendor/bundle/ruby/2.7.0/gems/nokogiri-1.10.10/ext/nokogiri/xml_document_fragment.h
@@ -0,0 +1,9 @@
+#ifndef NOKOGIRI_XML_DOCUMENT_FRAGMENT
+#define NOKOGIRI_XML_DOCUMENT_FRAGMENT
+
+#include <nokogiri.h>
+
+void init_xml_document_fragment();
+
+extern VALUE cNokogiriXmlDocumentFragment;
+#endif
