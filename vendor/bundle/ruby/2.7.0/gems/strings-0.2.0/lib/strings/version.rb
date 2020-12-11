162aaf24c336dbdcbfef7d7b76588d185203e24d
Denis Evers <denis-ev@users.noreply.github.com>
2020-12-11 13:22:39 +1000

    chore: remove vendor and add to gitignore
    Signed-off-by: Denis Evers <denis-ev@users.noreply.github.com>

--- vendor/bundle/ruby/2.7.0/gems/strings-0.2.0/lib/strings/version.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-module Strings
-  VERSION = "0.2.0"
-end # Strings
--- vendor/bundle/ruby/2.7.0/gems/tty-command-0.10.0/lib/tty/command/version.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module TTY
-  class Command
-    VERSION = "0.10.0"
-  end # Command
-end # TTY
