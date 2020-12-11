ce44cbaeb3f6be12ae884a382e37e1798db827a5
Kevin Poorman <kjp@codefriar.com>
2020-12-10 21:31:45 -0500

    finalizing terraform

--- tasks/terraform.rb
+++ tasks/terraform.rb
@@ -10,7 +10,19 @@ module Vlab
 
       say "Deploying cloud server".yellow
       run_playbook("playbook.terraform.yml", options)
+      run_docker("/bin/bash -c 'cd #{options[:config_dir]}; terraform init && terraform apply'")
       terraform_ip = run_docker "/bin/bash -c cd #{options[:config_dir]}; terraform show -json | jq -r .values.root_module.resources[0].values.ipv4_address"
+
+      say "Succesfully created a server at: #{terraform_ip}".green
+      say "Place this IP where you want it in your settings - either as vlab_ip or bastion.server_address".green
+      say "Then run vlab deploy to complete the setup.".green
+    end
+
+    desc "destroy", "Destroys server(s) creatd by terraform"
+    def destroy
+      say "Destroying terraform built server".red
+      run_docker("/bin/bash -c 'cd #{options[:config_dir]}; terraform destroy'")
+      say "Cloud server(s) destroyed.".green
     end
 
   end
