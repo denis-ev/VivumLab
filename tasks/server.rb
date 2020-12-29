c89ae36352f8296aee45f9c5b7e8e8b714021cff
Voryzen <piro.travesura@gmail.com>
2020-12-30 02:14:03 +1000

    refactoring dictionaries ssh, server

--- tasks/server.rb
+++ tasks/server.rb
@@ -4,21 +4,21 @@
 class Server < Thor
   include VlabI18n
 
-  desc 'shutdown', 'Executes a shutdown of the Vlab server, optionally with countdown'
+  desc I18n.t('server.shutdown.name'), I18n.t('server.shutdown.desc')
   option :time, type: :numeric, desc: 'Sets a countdown in minutes until the server shuts down'
   def shutdown
     # rubocop:disable Layout/LineLength
     execute_in_shell "ssh -i #{Dir.home}/.ssh/#{decrypted_config_file.passwordless_sshkey} #{decrypted_config_file.vlab_ssh_user}@#{decrypted_config_file.vlab_ip} -p #{decrypted_config_file.vlab_port} sudo shutdown #{options[:time]}"
     # rubocop:enable Layout/LineLength
-    say I18n.t('server.s_shutdown', time: options[:time]).yellow
+    say I18n.t('server.shutdown.out.shutdown', time: options[:time]).yellow
   end
 
-  desc 'restart', 'Executes a restart of the Vlab server, optionally with countdown'
+  desc I18n.t('server.restart.name'), I18n.t('server.restart.desc')
   option :time, type: :numeric, desc: 'Sets a countdown in minutes until the server restarts'
   def restart
     # rubocop:disable Layout/LineLength
     execute_in_shell "ssh -i #{Dir.home}/.ssh/#{decrypted_config_file.passwordless_sshkey} #{decrypted_config_file.vlab_ssh_user}@#{decrypted_config_file.vlab_ip} -p #{decrypted_config_file.vlab_port} sudo shutdown -r #{options[:time]}"
     # rubocop:enable Layout/LineLength
-    say I18n.t('server.s_restart', time: options[:time]).yellow
+    say I18n.t('server.restart.out.restart', time: options[:time]).yellow
   end
 end
--- tasks/ssh.rb
+++ tasks/ssh.rb
@@ -6,38 +6,38 @@ class Ssh < Thor
   include Utils
   include VlabI18n
 
-  desc 'launch', 'Opens a shell on your VivumLab server'
+  desc I18n.t('ssh.launch.name'), I18n.t('ssh.launch.desc')
   def launch
-    say I18n.t('ssh.s_launching').light_blue
+    say I18n.t('ssh.launch.out.launching').light_blue
     # rubocop:disable Layout/LineLength
     exec("ssh -i #{Dir.home}/.ssh/#{decrypted_config_file.passwordless_sshkey} #{decrypted_config_file.vlab_ssh_user}@#{decrypted_config_file.vlab_ip} -p #{decrypted_config_file.vlab_port}")
     # rubocop:enable Layout/LineLength
   end
 
-  desc 'create_sshkey', 'Creates an SSH key, if one doesn\'t exist'
+  desc I18n.t('ssh.create_sshkey.name'), I18n.t('ssh.create_sshkey.desc')
   def create_sshkey
-    say I18n.t('ssh.s_keyexists', home_dir: Dir.home, passwordless_sshkey: decrypted_config_file.passwordless_sshkey).yellow if ssh_key_exists?
+    say I18n.t('ssh.create_sshkey.out.keyexists', home_dir: Dir.home, passwordless_sshkey: decrypted_config_file.passwordless_sshkey).yellow if ssh_key_exists?
     return if ssh_key_exists?
 
-    say I18n.t('ssh.s_keycreating', passwordless_sshkey: decrypted_config_file.passwordless_sshkey).yellow
+    say I18n.t('ssh.create_sshkey.out.keycreating', passwordless_sshkey: decrypted_config_file.passwordless_sshkey).yellow
     # rubocop:disable Layout/LineLength
     execute_in_shell("ssh-keygen -q -N '' -C 'VivumLab@#{decrypted_config_file.domain}' -f #{Dir.home}/.ssh/#{decrypted_config_file.passwordless_sshkey}")
     # rubocop:enable Layout/LineLength
-    say I18n.t('ssh.s_keycreated').green
+    say I18n.t('ssh.create_sshkey.out.keycreated').green
   end
 
-  desc 'copy_sshkey', 'Copy your ssh key over to the server'
+  desc I18n.t('ssh.copy_sshkey.name'), I18n.t('ssh.copy_sshkey.desc')
   def copy_sshkey
-    say I18n.t('ssh.s_keycopying', vlab_ip: decrypted_config_file.vlab_ip).yellow
+    say I18n.t('ssh.copy_sshkey.out.keycopying', vlab_ip: decrypted_config_file.vlab_ip).yellow
     to_run = <<-EXECUTE
     ssh-copy-id -p #{decrypted_config_file.vlab_port} -i #{Dir.home}/.ssh/#{decrypted_config_file.passwordless_sshkey}.pub \
     #{decrypted_config_file.vlab_ssh_user}@#{decrypted_config_file.vlab_ip}
     EXECUTE
     execute_in_shell(to_run)
-    say I18n.t('ssh.s_keycopied').green
+    say I18n.t('ssh.copy_sshkey.out.keycopied').green
   end
 
-  desc 'setup', 'Creates and copies ssh keys to your server'
+  desc I18n.t('ssh.setup.name'), I18n.t('ssh.setup.desc')
   def setup
     invoke create_sshkey
     invoke copy_sshkey
