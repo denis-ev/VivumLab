09bdb605a373ba61b0cae0941b3ff572bfab1cad
Voryzen <piro.travesura@gmail.com>
2020-12-30 13:37:09 +1000

    fixing sanity_check dictionary vars

--- tasks/sanity_checks.rb
+++ tasks/sanity_checks.rb
@@ -22,15 +22,15 @@ class SanityChecks < Thor
     invoke 'check_for_git'
     invoke 'check_for_precommit'
 
-    say I18n.t('sanity_checks.local.passed').green
+    say I18n.t('sanity_checks.local.out.passed').green
   end
 
   desc I18n.t('sanity_checks.remote.name'), I18n.t('sanity_checks.remote.desc')
   def remote
-    say I18n.t('sanity_checks.remote.sshkeyverifying').yellow
+    say I18n.t('sanity_checks.remote.out.sshkeyverifying').yellow
     invoke check_ssh_keys
     invoke check_ssh_with_keys
-    say I18n.t('sanity_checks.remote.sshkeyverified').green
+    say I18n.t('sanity_checks.remote.out.sshkeyverified').green
   end
 
   desc I18n.t('sanity_checks.check_for_settings.name'), I18n.t('sanity_checks.check_for_settings.desc')
@@ -44,29 +44,29 @@ class SanityChecks < Thor
   def check_vault_pass
     return unless File.exist?('/vlab_vault_pass') && File.size('/vlab_vault_pass').zero?
 
-    say I18n.t('sanity_checks.check_vault_pass.vaultpassmissing', home_dir: Dir.home).red
-    say I18n.t('sanity_checks.check_vault_pass.vaultpasscreate').light_yellow
+    say I18n.t('sanity_checks.check_vault_pass.out.vaultpassmissing', home_dir: Dir.home).red
+    say I18n.t('sanity_checks.check_vault_pass.out.vaultpasscreate').light_yellow
 
-    decision = yes?(I18n.t('sanity_checks.check_vault_pass.vaultpasscreate'), :yellow)
+    decision = yes?(I18n.t('sanity_checks.check_vault_pass.out.vaultpasscreate'), :yellow)
     invoke 'core:generate_vault_pass' if decision
   end
 
   desc I18n.t('sanity_checks.check_for_git.name'), I18n.t('sanity_checks.check_for_git..desc')
   def check_for_git
     `which git`
-    say I18n.t('sanity_checks.check_for_git.gitnoexist').red unless $CHILD_STATUS.success?
+    say I18n.t('sanity_checks.check_for_git.out.gitnoexist').red unless $CHILD_STATUS.success?
   end
 
   desc I18n.t('sanity_checks.check_for_precommit.name'), I18n.t('sanity_checks.check_for_precommit.desc')
   def check_for_precommit
     if system('which pre-commit', out: File::NULL)
       if python_version >= REQUIRED_PYTHON_VERSION
-        say I18n.t('sanity_checks.check_for_precommit.lowprecommit', req_precommit_ver: REQUIRED_PRECOMMIT_VERSION).red if pre_commit_version <= REQUIRED_PRECOMMIT_VERSION
+        say I18n.t('sanity_checks.check_for_precommit.out.lowprecommit', req_precommit_ver: REQUIRED_PRECOMMIT_VERSION).red if pre_commit_version <= REQUIRED_PRECOMMIT_VERSION
       else
-        say I18n.t('sanity_checks.check_for_precommit.lowpython', req_python_ver: REQUIRED_PYTHON_VERSION).red
+        say I18n.t('sanity_checks.check_for_precommit.out.lowpython', req_python_ver: REQUIRED_PYTHON_VERSION).red
       end
     else
-      say I18n.t('sanity_checks.check_for_precommit.noprecommit').red
+      say I18n.t('sanity_checks.check_for_precommit.out.noprecommit').red
     end
   end
 
@@ -75,7 +75,7 @@ class SanityChecks < Thor
       # rubocop:disable Layout/LineLength
       execute_in_shell "ssh -q -o StrictHostKeyChecking=no -o ConnectTimeout=3 #{decrypted_config_file['VLAB_SSH_USER']}@#{decrypted_config_file['VLAB_IP']} exit"
       # rubocop:enable Layout/LineLength
-      say I18n.t('sanity_checks.check_ssh_with_keys.sshunable').red unless $CHILD_STATUS.success?
+      say I18n.t('sanity_checks.check_ssh_with_keys.out.sshunable').red unless $CHILD_STATUS.success?
     end
   end
 end
