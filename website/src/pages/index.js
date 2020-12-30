2272f18c41ccb6cec7dfbcbecc5ef39c8e7b54dc
Denis Evers <denis-ev@users.noreply.github.com>
2020-12-30 12:44:25 +1000

    chore(website): update website home
    Signed-off-by: Denis Evers <denis-ev@users.noreply.github.com>

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -11,25 +11,88 @@ import TabItem from '@theme/TabItem';
 
 const installs = [
   {
-    label: 'git',
+    label: 'run',
+    snippet: `# Start the CLI
+./launch.sh`
+  },
+  {
+    label: 'install',
+    snippet: `# Install
+git clone --branch master https://github.com/VivumLab/VivumLab.git
+
+# Start the CLI
+./launch.sh
+
+# Run within the started Docker container
+vlab core deploy`
+  },
+  {
+    label: 'dev-install',
+    snippet: `# Developer Install
+git clone --branch dev https://github.com/VivumLab/VivumLab.git
+
+# Start the CLI with dev parameter
+./launch.sh dev
+
+# Run within the started Docker container
+vlab core deploy`
+  },
+  {
+    label: 'local-build',
     snippet: `# Install
 git clone https://github.com/VivumLab/VivumLab.git
 
-# Make a config
-./vlab config
+# Build / Start the CLI
+./launch.sh local
 
-# Run
-./vlab deploy`
+# Start the CLI (Only works if image was build before)
+./launch.sh local start
+
+# Run within the started Docker container
+vlab core deploy`
   }
 ]
 
 const snippets = [
   {
-    label: 'Enable Services',
+    label: 'Deploy',
     further: '/docs/core/Installation#enabling-services',
-    config: `vlab set SERVICENAME.enable=true
+    config: `vlab core deploy`,
+  },
+  {
+    label: 'Enable / Update Service',
+    further: '/docs/core/Installation#enabling-services',
+    config: `# Enable a service
+vlab service enable SERVICENAME
 
-vlab deploy`,
+# Update a service
+vlab update --service SERVICENAME
+
+# Show config for a service
+vlab config show --service SERVICENAME
+
+# Change options for a service (Example for option https_only)
+vlab config set --config-key=SERVICENAME.https_only --value=true
+
+# Options Examples
+SERVICENAME.enable = true/false
+SERVICENAME.https_only = true/false
+SERVICENAME.auth = true/false
+SERVICENAME.domain = false or "example.com"
+SERVICENAME.subdomain = "SERVICENAME"
+SERVICENAME.version = "latest"
+`,
+  },
+  {
+    label: 'Update all Services',
+    further: '/docs/core/Installation#enabling-services',
+    config: `vlab update`,
+  },
+  {
+    label: 'Show config',
+    further: '/docs/core/Installation#enabling-services',
+    config: `# Show config for a service
+vlab config show --service SERVICENAME`,
   }
 ];
 
@@ -74,8 +137,8 @@ function Home() {
   return (
     <Layout
       title={`${siteConfig.title}`}
-      description="The stream processor for mundane tasks"
-      keywords={["benthos","stream processor","data engineering","ETL","ELT","event processor","go","golang"]}>
+      description="It's your data, it's your life. Own it, with VivumLab"
+      keywords={["VivumLab","selfhosting","self hosting","privacy","homelab"]}>
       <header className={classnames('hero', styles.heroBanner)}>
         <div className="container">
           <div className="row">
