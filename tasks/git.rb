94c626532f3a99403bbafaa24068ece83b4e11b0
Kevin Poorman <kjp@codefriar.com>
2021-01-03 04:33:07 +0000

    rubocop style updates

--- tasks/git.rb
+++ tasks/git.rb
@@ -11,14 +11,11 @@ class Git < Thor
   desc I18n.t('git.sync.usage'), I18n.t('git.sync.desc')
   option :disable_push, required: false, type: :boolean, desc: I18n.t('options.disablegitsync'), default: false
   def sync
-    if Dir.exist? "./#{options[:config_dir]}/.git"
-      say I18n.t('git.sync.out.settingssyncing').yellow
-      ensure_precommit
-      execute_git_sync options[:config_dir], options[:disable_push]
-      say I18n.t('git.sync.out.settingssynced').green
-    else
-      say I18n.t('git.sync.out.notsetup').red
-    end
+    reject_if_dir_not_present unless dir_exists
+
+    say I18n.t('git.sync.out.settingssyncing').yellow
+    execute_git_sync options[:config_dir], options[:disable_push]
+    say I18n.t('git.sync.out.settingssynced').green
   end
 
   desc I18n.t('git.track.usage'), I18n.t('git.track.desc')
@@ -29,6 +26,12 @@ class Git < Thor
   end
 
   no_commands do
+    def reject_if_dir_not_present
+      dir_exists = Dir.exist? "./#{options[:config_dir]}/.git"
+      say I18n.t('git.sync.out.notsetup').red unless dir_exists
+      exit 1 unless dir_exists
+    end
+
     def ensure_precommit
       FileUtils.mkdir_p "#{options[:config_dir]}/.git/hooks"
       FileUtils.cp 'git_sync_pre_commit', "#{options[:config_dir]}/.git/hooks/pre-commit"
@@ -36,11 +39,12 @@ class Git < Thor
     end
 
     def execute_git_sync(config_dir, disable_push)
+      ensure_precommit
       execute_in_shell("git config --global user.email \"#{decrypted_config_file['admin_email']}\"")
       execute_in_shell("git config --global user.usage \"#{decrypted_config_file['default_username']}\"")
       execute_in_shell('git pull', config_dir) unless disable_push
       execute_in_shell('git add *', config_dir)
-      execute_in_shell('git commit -a -m "updating settings"', config_dir, true )
+      execute_in_shell('git commit -a -m "updating settings"', config_dir, true)
       execute_in_shell('git push', config_dir) unless disable_push
     rescue Subprocess::NonZeroExit => e
       say 'Failed to execute some portion of the git sync.'.red
