8ca4dc747c5ad4a8467b9acfa66315e97adc88ba
Kevin Poorman <kjp@codefriar.com>
2021-01-03 04:48:54 +0000

    refactor to rubocop styles

--- tasks/migration.rb
+++ tasks/migration.rb
@@ -6,7 +6,8 @@ class Migration < Thor
   include Utils
   include VlabI18n
 
-  class_option :debug, desc: I18n.t('options.ansibledebug'), enum: %w[none warn debug trace], default: :none, aliases: ['-d']
+  class_option :debug, desc: I18n.t('options.ansibledebug'),
+                       enum: %w[none warn debug trace], default: :none, aliases: ['-d']
   class_option :config_dir, type: :string, desc: I18n.t('options.configdir'), default: 'settings'
 
   desc I18n.t('migration.single_config.usage'), I18n.t('migration.single_config.desc')
@@ -15,21 +16,32 @@ class Migration < Thor
     # * There is no encrypted.yml in the current settings folder
     # * There is a vault.yml in the current settings folder
     # * There is a config.yml in the current settings folder
-    if (File.exist? "#{options[:config_dir]}/config.yml") &&
-       (File.exist? "#{options[:config_dir]}/vault.yml") &&
-       (!File.exist? "#{options[:config_dir]}/encrypted.yml")
+    if meets_single_config_conditions?
       invoke 'deprecated:vault_decrypt', [], {}
-      config_file ||= ConfigFile.new(YAML.load_file("#{options[:config_dir]}/config.yml"))
-      vault_file ||= ConfigFile.new(YAML.load_file("#{options[:config_dir]}/vault.yml"))
-
-      @decrypted_config_file = config_file.merge vault_file.vault
-      @decrypted_config_file.ansible_become_password = vault_file.ansible_become_password
-      @decrypted_config_file.default_password = vault_file.default_password
+      execute_single_config_migration
       save_config_file
-      FileUtils.rm "#{options[:config_dir]}/config.yml"
-      FileUtils.rm "#{options[:config_dir]}/vault.yml"
+      cleanup
     else
       say I18n.t('migration.single_config.out.notdone').light_blue
     end
+  end
+
+  def execute_single_config_migration(config_file, vault_file)
+    config_file ||= ConfigFile.new(YAML.load_file("#{options[:config_dir]}/config.yml"))
+    vault_file ||= ConfigFile.new(YAML.load_file("#{options[:config_dir]}/vault.yml"))
+    @decrypted_config_file = config_file.merge vault_file.vault
+    @decrypted_config_file.ansible_become_password = vault_file.ansible_become_password
+    @decrypted_config_file.default_password = vault_file.default_password
+  end
+
+  def meets_single_config_conditions?
+    (File.exist? "#{options[:config_dir]}/config.yml") &&
+      (File.exist? "#{options[:config_dir]}/vault.yml") &&
+      (!File.exist? "#{options[:config_dir]}/encrypted.yml")
+  end
+
+  def cleanup
+    FileUtils.rm "#{options[:config_dir]}/config.yml"
+    FileUtils.rm "#{options[:config_dir]}/vault.yml"
   end
 end
