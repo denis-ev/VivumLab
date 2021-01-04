c8a87e7d5f31f110a4a3e7ac447269a74c6dbcd8
Kevin Poorman <kjp@codefriar.com>
2021-01-04 16:40:05 +0000

    cleaning up terraform

--- tasks/terraform.rb
+++ tasks/terraform.rb
@@ -8,29 +8,9 @@ class Terraform < Thor
 
   desc I18n.t('terraform.create.usage'), I18n.t('terraform.create.desc')
   def create
-    invoke 'git:sync'
-    invoke 'config:new'
-    say I18n.t('terraform.create.out.creating').yellow
-    if decrypted_config_file['do_access_token'] == false
-      say 'No valid DO access token found.'
-      say "Please run 'vlab dev set --dev --config-key do_access_token --value <token>'"
-      say 'before attempting terraform.'.red
-      exit 1
-    end
-    run_playbook('playbook.terraform.yml', options)
-    FileUtils.mv 'provider.tf', "settings/#{options[:config_dir]}/provider.tf"
-    FileUtils.mv 'cloud_vivumlab.tf', "settings/#{options[:config_dir]}/cloud_vivumlab.tf"
-    FileUtils.mv 'terraform.tfvars', "settings/#{options[:config_dir]}/terraform.tfvars"
-    execute_in_shell('terraform init', "settings/#{options[:config_dir]}")
-    execute_in_shell('terraform apply -auto-approve', "settings/#{options[:config_dir]}")
-    terraform_json = Subprocess.check_output('terraform show -json', "settings/#{options[:config_dir]}")
-    terraform_ip = Hashie::Mash.new(JSON.parse(terraform_json))
-                               .values
-                               .last
-                               .root_module.resources[0]
-                               .values.select{ |y| y.instance_of? Hashie::Mash }
-                               .first
-                               .ipv4_address
+    exit 1 unless prerequisites_met?
+    terraform_ansible_setup(options)
+    terraform_ip = terraform_shell_commands(options)
     say I18n.t('terraform.create.out.created', ip: terraform_ip).green
     say I18n.t('terraform.create.out.setip', ip: terraform_ip, config_dir: options[:config_dir]).light_blue
   end
@@ -45,5 +25,43 @@ class Terraform < Thor
       FileUtils.rm("settings/#{options[:config_dir]}/#{file}") if File.exist? "settings/#{options[:config_dir]}/#{file}"
     end
     say I18n.t('terraform.destroy.out.destroyed').green
+  end
+
+  no_commands do
+    def prerequisites_met?
+      invoke 'git:sync'
+      invoke 'config:new'
+      say I18n.t('terraform.create.out.creating').yellow
+      if decrypted_config_file['do_access_token'] == false
+        say 'No valid DO access token found.'
+        say "Please run 'vlab dev set --dev --config-key do_access_token --value <token>'"
+        say 'before attempting terraform.'.red
+      end
+      decrypted_config_file['do_access_token']
+    end
+
+    def extract_ip_from_terraform_json(json)
+      Hashie::Mash.new(JSON.parse(json))
+                  .values
+                  .last
+                  .root_module.resources[0]
+                  .values.select { |y| y.instance_of? Hashie::Mash }
+                  .first
+                  .ipv4_address
+    end
+
+    def terraform_ansible_setup(options)
+      run_playbook('playbook.terraform.yml', options)
+      FileUtils.mv 'provider.tf', "settings/#{options[:config_dir]}/provider.tf"
+      FileUtils.mv 'cloud_vivumlab.tf', "settings/#{options[:config_dir]}/cloud_vivumlab.tf"
+      FileUtils.mv 'terraform.tfvars', "settings/#{options[:config_dir]}/terraform.tfvars"
+    end
+
+    def terraform_shell_commands(options)
+      execute_in_shell('terraform init', "settings/#{options[:config_dir]}")
+      execute_in_shell('terraform apply -auto-approve', "settings/#{options[:config_dir]}")
+      terraform_json = Subprocess.check_output(%w[terraform show -json], "settings/#{options[:config_dir]}")
+      extract_ip_from_terraform_json(terraform_json)
+    end
   end
 end
