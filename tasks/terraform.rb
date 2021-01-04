00488a0bbf2e32d2123d9342b8c80df8fdaa528e
Kevin Poorman <kjp@codefriar.com>
2021-01-04 16:26:45 +0000

    updates to git and terraform namespaces

--- tasks/terraform.rb
+++ tasks/terraform.rb
@@ -4,7 +4,7 @@
 class Terraform < Thor
   include VlabI18n
   include Utils
-  require 'open3'
+  require 'json'
 
   desc I18n.t('terraform.create.usage'), I18n.t('terraform.create.desc')
   def create
@@ -21,33 +21,16 @@ class Terraform < Thor
     FileUtils.mv 'provider.tf', "settings/#{options[:config_dir]}/provider.tf"
     FileUtils.mv 'cloud_vivumlab.tf', "settings/#{options[:config_dir]}/cloud_vivumlab.tf"
     FileUtils.mv 'terraform.tfvars', "settings/#{options[:config_dir]}/terraform.tfvars"
-    # @todo terraform create check functions
-    # @body 'vlab terraform create' works, but code needs to be verified
-    # terraform_init = "/bin/bash -c 'exit 0'"
-    # terraform_init = "/bin/bash -c 'cd settings/#{options[:config_dir]} && terraform init'" unless Dir.exist? "settings/#{options[:config_dir]}/.terraform"
     execute_in_shell('terraform init', "settings/#{options[:config_dir]}")
-    # Open3.popen2e(terraform_init) do |stdin, stdout_and_stderr, wait_thr|
-    #   while line = stdout_and_stderr.gets
-    #     puts line
-    #   end
-    # end
-    # terraform_apply = "/bin/bash -c 'cd settings/#{options[:config_dir]} && terraform apply -auto-approve'"
     execute_in_shell('terraform apply -auto-approve', "settings/#{options[:config_dir]}")
-    # Open3.popen2e(terraform_apply) do |stdin, stdout_and_stderr, wait_thr|
-    #   while line = stdout_and_stderr.gets
-    #     puts line
-    #   end
-    # end
-    # rubocop:disable Layout/LineLength
-    # terraform_get_ip = "/bin/bash -c 'cd settings/#{options[:config_dir]} && terraform show -json | jq -r .values.root_module.resources[0].values.ipv4_address'"
-    # terraform_ip = ""
-    # Open3.popen2e(terraform_get_ip) do |stdin, stdout_and_stderr, wait_thr|
-    #   while line = stdout_and_stderr.gets
-    #     terraform_ip = line
-    #   end
-    # end
-    execute_in_shell('terraform show -json | jq -r .valuse.root_module.resources[0].values.ipv4_address', "settings/#{options[:config_dir]}")
-    # rubocop:enable Layout/LineLength
+    terraform_json = Subprocess.check_output('terraform show -json', "settings/#{options[:config_dir]}")
+    terraform_ip = Hashie::Mash.new(JSON.parse(terraform_json))
+                               .values
+                               .last
+                               .root_module.resources[0]
+                               .values.select{ |y| y.instance_of? Hashie::Mash }
+                               .first
+                               .ipv4_address
     say I18n.t('terraform.create.out.created', ip: terraform_ip).green
     say I18n.t('terraform.create.out.setip', ip: terraform_ip, config_dir: options[:config_dir]).light_blue
   end
@@ -55,12 +38,6 @@ class Terraform < Thor
   desc I18n.t('terraform.destroy.usage'), I18n.t('terraform.destroy.desc')
   def destroy
     say I18n.t('terraform.destroy.out.destroying').yellow
-    # terraform_destroy = "/bin/bash -c 'cd settings/#{options[:config_dir]} && terraform destroy -auto-approve'"
-    # Open3.popen2e(terraform_destroy) do |stdin, stdout_and_stderr, wait_thr|
-    #   while line = stdout_and_stderr.gets
-    #     puts line
-    #   end
-    # end
     execute_in_shell('terraform destroy -auto-approve', "settings/#{options[:config_dir]}")
     files = %w[provider.tf cloud_vivumlab.tf terrraform.tfvars terraform.lock.hcl
                terraform.tfstate terraform.tfstate.backup]
