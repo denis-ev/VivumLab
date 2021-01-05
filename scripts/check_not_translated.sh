2dbca8b3cefe84406bc545725fcc53f56bc224f5
Denis Evers <denis-ev@users.noreply.github.com>
2021-01-05 17:46:52 +1000

    chore: improve check_not_translated pre-commit
    Signed-off-by: Denis Evers <denis-ev@users.noreply.github.com>

--- scripts/check_not_translated.sh
+++ scripts/check_not_translated.sh
@@ -1,6 +1,44 @@
 #!/usr/bin/env bash
 
-! grep -R --include='*.rb' --include='vlab' --exclude='demo_tasks.rb' 'puts ' . | grep -n "puts \'"
-! grep -R --include='*.rb' --include='vlab' --exclude='demo_tasks.rb' 'puts ' . | grep -n 'puts \"'
-! grep -R --include='*.rb' --include='vlab' --exclude='demo_tasks.rb' 'say ' . | grep -n "say \'"
-! grep -R --include='*.rb' --include='vlab' --exclude='demo_tasks.rb' 'say ' . | grep -n 'say \"'
+# Bash Linter for Pre-commits
+#
+# Exit 0 if no errors found
+# Exit 1 if errors were found
+
+# Flag to denote if a error was found
+erros_found=false
+
+error_count=0
+for file in $@
+do
+  ! grep -R 'puts ' $file | grep -n "puts \'"
+  if [ $? -ne 0 ]; then
+    # echo "Errors were detected" >&2
+    error_count=$[$error_count +1]
+    erros_found=true
+  fi
+  ! grep -R 'puts ' $file | grep -n 'puts \"'
+  if [ $? -ne 0 ]; then
+    # echo "Errors were detected" >&2
+    error_count=$[$error_count +1]
+    erros_found=true
+  fi
+  ! grep -R 'say ' $file | grep -n "say \'"
+  if [ $? -ne 0 ]; then
+    # echo "Errors were detected" >&2
+    error_count=$[$error_count +1]
+    erros_found=true
+  fi
+  ! grep -R 'say ' $file | grep -n 'say \"'
+  if [ $? -ne 0 ]; then
+    # echo "Errors were detected" >&2
+    error_count=$[$error_count +1]
+    erros_found=true
+  fi
+done
+
+if [ "$erros_found" = true ]; then
+    exit 1
+fi
+
+exit 0
