ca6dfecee94a21d163dbe63f7175a9c3bea40a7c
Denis Evers <denis-ev@users.noreply.github.com>
2021-01-05 18:06:37 +1000

    refactor: remove unused code from check_not_translated.sh
    Signed-off-by: Denis Evers <denis-ev@users.noreply.github.com>

--- scripts/check_not_translated.sh
+++ scripts/check_not_translated.sh
@@ -5,39 +5,15 @@
 # Exit 0 if no errors found
 # Exit 1 if errors were found
 
-# Flag to denote if a error was found
-erros_found=false
-
-error_count=0
 for file in $@
 do
   ! grep -R 'puts ' $file | grep -n "puts \'"
-  if [ $? -ne 0 ]; then
-    # echo "Errors were detected" >&2
-    error_count=$[$error_count +1]
-    erros_found=true
-  fi
   ! grep -R 'puts ' $file | grep -n 'puts \"'
-  if [ $? -ne 0 ]; then
-    # echo "Errors were detected" >&2
-    error_count=$[$error_count +1]
-    erros_found=true
-  fi
   ! grep -R 'say ' $file | grep -n "say \'"
-  if [ $? -ne 0 ]; then
-    # echo "Errors were detected" >&2
-    error_count=$[$error_count +1]
-    erros_found=true
-  fi
   ! grep -R 'say ' $file | grep -n 'say \"'
-  if [ $? -ne 0 ]; then
-    # echo "Errors were detected" >&2
-    error_count=$[$error_count +1]
-    erros_found=true
-  fi
 done
 
-if [ "$erros_found" = true ]; then
+if [ $? -ne 0 ]; then
     exit 1
 fi
 
