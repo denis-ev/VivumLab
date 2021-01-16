979c851dac3d6333832248037d52cfe0ad367ce4
Denis Evers <denis-ev@users.noreply.github.com>
2021-01-16 12:37:09 +1000

    chore: fixes
    Signed-off-by: Denis Evers <denis-ev@users.noreply.github.com>

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,15 +2,12 @@
 
 DEFAULT_PATH="/data"
 
-echo -e "Variables:
-\\t- UID=${TARGET_UID}
-\\t- GID=${TARGET_GID}
-\\t- VERSION=${VERSION}"
-
 echo "Install vivumlab..."
 cd $DEFAULT_PATH
-git checkout $VERSION &
-[[ "$!" -gt 0 ]] && wait $!
+if [[ $VERSION != 'local' ]]; then
+  git checkout $VERSION &
+  [[ "$!" -gt 0 ]] && wait $!
+fi
 chown -R "$TARGET_UID":"$TARGET_GID" "$DEFAULT_PATH"
 
 bundle install
