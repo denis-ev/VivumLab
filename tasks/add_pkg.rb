469826328092c312b518d31a249046e16c131fba
Kevin Poorman <kjp@codefriar.com>
2021-01-28 03:47:16 +0000

    updates

--- tasks/add_pkg.rb
+++ tasks/add_pkg.rb
@@ -183,7 +183,7 @@ class AddPkg < Thor
       lines.dup.each_with_index do |line, index|
         next unless line.strip == "#{next_name}:"
 
-        lines.insert index, config_block(to_insert)
+        lines.insert index, to_insert
       end
       File.open('website/sidebars.js', 'w+') do |f|
         f.puts(lines)
