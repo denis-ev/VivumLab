7be660fe062423286bc6f5473831083493cbde24
Kevin Poorman <kjp@codefriar.com>
2021-02-03 02:01:39 +0000

    chowning

--- tasks/add_pkg.rb
+++ tasks/add_pkg.rb
@@ -17,7 +17,7 @@ class AddPkg < Thor
     add_docs_to_docusaurus
     add_service_to_group_vars_all
     add_to_config_template
-    FileUtils.chown_R '1000', '1000', '*'
+    FileUtils.chown_R '1000', '1000', '/data'
   end
 
   no_commands do
